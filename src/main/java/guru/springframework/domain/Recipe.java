4f1e88518e64082553f5f2effcce81394a539f15
egarcia405 <elias.garcia.405@gmail.com>
2021-01-30 12:33:34 -0500

    adding domains

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,15 @@
+package guru.springframework.domain;
+
+public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+    //private Difficulty difficulty;
+    private Byte[] image;
+}
